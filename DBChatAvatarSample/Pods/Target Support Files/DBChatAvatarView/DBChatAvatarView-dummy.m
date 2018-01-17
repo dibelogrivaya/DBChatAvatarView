7083e03fc961e3c181de4be994757effac964343
Di Belogrivaya <diana.belogrivaya@gmail..com>
2018-01-17 18:28:48 +0300

    Remove Pods folder.

--- DBChatAvatarSample/Pods/Target Support Files/DBChatAvatarView/DBChatAvatarView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DBChatAvatarView : NSObject
-@end
-@implementation PodsDummy_DBChatAvatarView
-@end
--- DBChatAvatarSample/Pods/Target Support Files/Pods-DBChatAvatarSample/Pods-DBChatAvatarSample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DBChatAvatarSample : NSObject
-@end
-@implementation PodsDummy_Pods_DBChatAvatarSample
-@end
